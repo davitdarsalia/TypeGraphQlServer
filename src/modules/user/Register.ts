25e6960d487037e72a72665cb71fd270489570f2
davitdarsalia <darsalia.david1998@gmail.com>
2022-11-03 14:41:57 +0400

    feat: Base Resolvers <Temp>

---
+++ src/modules/user/Register.ts
@@ -0,0 +1,44 @@
+import * as bcrypt from "bcryptjs";
+import {
+  Arg,
+  FieldResolver,
+  Mutation,
+  Query,
+  Resolver,
+  Root,
+  UseMiddleware,
+} from "type-graphql";
+
+import { User } from "../../entities/user.entity";
+import { RegisterInput } from "./registerInput.inputType";
+import { isAuth } from "../../utils/jwt";
+
+@Resolver(User)
+export class RegisterResolver {
+  @UseMiddleware(isAuth)
+  @Query(() => String)
+  async hello() {
+    return "S";
+  }
+
+  @FieldResolver(() => String)
+  async name(@Root() parent: User) {
+    return `${parent.firstName} ${parent.lastName}`;
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg("userInput")
+    { email, firstName, lastName, secretKey, password }: RegisterInput
+  ): Promise<User> {
+    const user = User.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      secretKey: secretKey,
+      password: await bcrypt.hash(password, 12),
+    }).save();
+
+    return user;
+  }
+}
---
+++ src/modules/user/login.ts
@@ -0,0 +1,80 @@
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../../entities/user.entity";
+import * as bcrypt from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { LoginInput } from "./login.inputType";
+
+import { GraphQlLoginResponse, loginResponse } from "../../types/loginTypes";
+
+export const signature = "nd1infi84ng82n8f1n39ef953n4g8gn";
+const refreshSignature =
+  "fm294gj49gj924rj3g89n4g8n29gjnm92jgn95nh94nnhg4in3n9h4ihn3ignri3";
+
+@Resolver()
+export class LoginResolver {
+  @Mutation(() => GraphQlLoginResponse, { nullable: true })
+  async login(
+    @Arg("loginInput") { email, password }: LoginInput
+  ): Promise<loginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    const validPassword = await bcrypt.compare(password, user!.password);
+
+    if (!user) {
+      return {
+        userID: -1,
+        accessToken: "",
+        accessTokenExpiryTime: 7,
+        refreshToken: "",
+        refreshTokenExpiryTime: 7,
+        code: 404,
+      };
+    }
+
+    if (!validPassword) {
+      return {
+        userID: -1,
+        accessToken: "",
+        accessTokenExpiryTime: 7,
+        refreshToken: "",
+        refreshTokenExpiryTime: 7,
+        code: 404,
+      };
+    }
+
+    const accessToken = sign(
+      {
+        userId: user.id,
+        email: user.email,
+        issuedAt: new Date().toLocaleDateString(),
+      },
+      signature,
+      {
+        expiresIn: "1m",
+      }
+    );
+
+    const refreshToken = sign(
+      {
+        userId: user.id,
+        email: user.email,
+        issuedAt: new Date().toLocaleDateString(),
+      },
+      refreshSignature,
+      {
+        expiresIn: "1m",
+      }
+    );
+
+    const successObj: loginResponse = {
+      userID: user.id,
+      accessToken,
+      accessTokenExpiryTime: 7,
+      refreshToken,
+      refreshTokenExpiryTime: 7,
+      code: 200,
+    };
+
+    return successObj;
+  }
+}
