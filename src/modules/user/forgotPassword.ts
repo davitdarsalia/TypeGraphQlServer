e4dd05c21e451bb02d690cc95aa75537f3717aa0
davitdarsalia <darsalia.david1998@gmail.com>
2022-11-04 20:04:33 +0400

    TODO: Complete Forgot Pass Mutation

--- src/modules/user/forgotPassword.ts
+++ src/modules/user/forgotPassword.ts
@@ -1,28 +1,50 @@
 import * as bcrypt from "bcryptjs";
 import { Arg, Mutation, Resolver } from "type-graphql";
+import { GlobalRedisClient } from "../../../redis";
 
 import { User } from "../../entities/user.entity";
-import { ForgotPasswordInput } from "./forgotPassword.inputType";
+import { OtpGenerator } from "../../utils/otpGenerator";
 
 @Resolver()
 export class ForgotPasswordResolver {
-  @Mutation(() => Boolean)
-  async forgotPassword(
-    @Arg("input")
-    { email, password, newPassword }: ForgotPasswordInput
+  @Mutation(() => String)
+  async sendPasswordRecoveryEmail(
+    @Arg("email") email: string
   ): Promise<string> {
     const user = await User.findOne({
-      where: { email, password },
+      where: { email },
     });
 
     if (!user) {
       return "User Doesn't Found. Try Again";
     }
 
-    // Step 2 - Send Confirmation Code And If === , Update An User
-    user.password = await bcrypt.hash(newPassword, 12);
-    await user.save();
+    const resetOTP = OtpGenerator();
+    console.log(resetOTP);
+    GlobalRedisClient.set("otp", resetOTP);
 
-    return "Password Had Been Successfully Reset";
+    return "OTP Has Been Sent To Your Email";
+  }
+
+  @Mutation(() => User)
+  async resetPassword(
+    @Arg("email") email: string,
+    @Arg("newPassword") newPassword: string,
+    @Arg("otp") otp: string
+  ): Promise<void | boolean | number> {
+    // let user = await User.findOne({
+    //   where: { email },
+    // });
+
+    // const existingOTP = await GlobalRedisClient.get("otp");
+    // if (user && existingOTP === otp) {
+    //   user.password = await bcrypt.hash(newPassword, 12);
+    //   user.save();
+    //   GlobalRedisClient.del("otp");
+
+    //   return true;
+    // }
+
+    return false;
   }
 }
