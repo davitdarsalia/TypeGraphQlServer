b65ea19dd9c49885f02518af186020be96a852e8
davitdarsalia <darsalia.david1998@gmail.com>
2022-11-04 20:05:06 +0400

    feat: Updated Login

--- src/modules/user/login.ts
+++ src/modules/user/login.ts
@@ -1,15 +1,10 @@
-import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Resolver } from "type-graphql";
 import { User } from "../../entities/user.entity";
 import * as bcrypt from "bcryptjs";
-import { sign } from "jsonwebtoken";
+
 import { LoginInput } from "./login.inputType";
-
 import { GraphQlLoginResponse, loginResponse } from "../../types/loginTypes";
-
-export const signature = "nd1infi84ng82n8f1n39ef953n4g8gn";
-const refreshSignature =
-  "fm294gj49gj924rj3g89n4g8n29gjnm92jgn95nh94nnhg4in3n9h4ihn3ignri3";
-
+import { SignToken } from "../../utils/jwt";
 @Resolver()
 export class LoginResolver {
   @Mutation(() => GraphQlLoginResponse, { nullable: true })
@@ -32,29 +27,8 @@ export class LoginResolver {
       return "User Is Not Confirmed. Try Confirm Yourself Via Address";
     }
 
-    const accessToken = sign(
-      {
-        userId: user.id,
-        email: user.email,
-        issuedAt: new Date().toLocaleDateString(),
-      },
-      signature,
-      {
-        expiresIn: "1m",
-      }
-    );
-
-    const refreshToken = sign(
-      {
-        userId: user.id,
-        email: user.email,
-        issuedAt: new Date().toLocaleDateString(),
-      },
-      refreshSignature,
-      {
-        expiresIn: "1m",
-      }
-    );
+    const accessToken = SignToken(user, "Access");
+    const refreshToken = SignToken(user, "Refresh");
 
     const successObj: loginResponse = {
       userID: user.id,
